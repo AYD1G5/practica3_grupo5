02b9f37e4e088d35966f1d3dab358c5d12806320
deahtom123 <201504200@ingenieria.usac.edu.gt>
2018-11-02 18:53:08 -0600

    V4.0.5
    Realizar pago

--- app/Http/Controllers/NewFuncionController.php
+++ app/Http/Controllers/NewFuncionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use DB;
 use Auth;
 use App\Funciones;
@@ -19,6 +20,34 @@ class NewFuncionController extends Controller
     {
         //    return view('CrearGrupo.CrearGrupo');
 
+    }
+    public function pago(Request $request)
+    {
+        $carrito_usuario=DB::table('carrito as c')
+        ->where('c.id_user', '=', Auth::id())
+        ->first();
+        $productoscarrito=DB::table('carrito_producto as cp')
+        ->where('cp.id_carrito', '=', $carrito_usuario->id_carrito)
+        ->get();     
+        $total = 0;
+        foreach($productoscarrito as $pcar){
+                $pcar->subtotal = $subtotal = $this->func->calcularSubTotal($pcar->cantidad, $pcar->precio);    
+                $total = $total + $pcar->subtotal;
+            
+        }
+        $pago = $request->input('pago');
+        $vuelto=$this->vuelto($total,$pago);
+        if($vuelto==-1)
+        {
+            die("Error");
+        }
+        else
+        {
+            //die($vuelto);
+            return Redirect::to('/FinalizarCompra');
+        }
+
+        
     }
     public function realizarpago()
     {
--- resources/views/Realizar_pago.blade.php
+++ resources/views/Realizar_pago.blade.php
@@ -10,8 +10,12 @@
 
   <hr class="my-3">
   
+    <form method="POST">
+
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 		<h3><strong>Total: {{number_format((float)$total, 2, '.', '')}} </strong></h3>
-        <h3><p>Pago:<input type=text name="pagar"></h3>
+        <h3><p>Pago:<input type=text name="pago"></h3>
         <button type="submit" class="btn btn-primary">Pagar</button>
     </div>
+    </form>
     @endsection
